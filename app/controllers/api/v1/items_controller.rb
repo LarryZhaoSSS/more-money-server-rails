ec45e205fea666c9638601e4b6001ebf519d450f
LarryZhao <larryzhao1994@outlook.com>
2022-04-30 20:20:34 +0800

    kaminari page

---
+++ app/controllers/api/v1/items_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::ItemsController < ApplicationController
+    def index
+       items = Item.page params[:page]
+       render json: {resources:items}
+    end
+    def create
+        item = Item.new amount: 1
+        if item.save
+            render json: {resource:item}
+        else 
+            render json:{errors:item.errors}
+        end
+    end
+end
